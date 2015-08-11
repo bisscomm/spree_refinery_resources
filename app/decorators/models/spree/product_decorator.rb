990eb13c057e58932a00f7ea4ca4d495cc303499
Brice Sanchez <brice@bisscomm.com>
2015-08-11 11:18:35 -0400

    Add dependent: :destroy on has_many relations

--- app/decorators/models/spree/product_decorator.rb
+++ app/decorators/models/spree/product_decorator.rb
@@ -1,4 +1,4 @@
 Spree::Product.class_eval do
-  has_many :product_resources, class_name: 'Spree::ProductResource', :foreign_key => :product_id
+  has_many :product_resources, class_name: 'Spree::ProductResource', dependent: :destroy, foreign_key: :product_id
   has_many :resources, through: :product_resources, class_name: 'Refinery::Resource'
 end
